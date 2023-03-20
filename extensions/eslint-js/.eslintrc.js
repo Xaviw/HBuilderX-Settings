8bcb032675d6531218db3ed8b558ee56a61e511c
xavi <xaviw@foxmail.com>
2023-03-20 20:47:46 +0800

    update

--- extensions/eslint-js/.eslintrc.js
+++ extensions/eslint-js/.eslintrc.js
@@ -1,38 +1,38 @@
 //详细配置教程请参考：http://eslint.cn/docs/user-guide/configuring
 module.exports = {
-    "plugins": [
-        "html",
-    ],
-    "env": {
-      "browser": true,
-      "node": true,
-      "commonjs": true,
+  plugins: ['html'],
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  globals: {
+    uni: 'writable',
+    plus: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "allowImportExportEverywhere": false,
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-      "array-callback-return": 1,
-      "no-floating-decimal": 1,
-      "init-declarations": 1,
-      "no-use-before-define": 2,
-      "array-bracket-spacing": 1,
-      "block-spacing": 1,
-      "comma-dangle": ["error", {
-          "arrays": "always",
-          "objects": "always",
-      }],
-      "eol-last": 1,
-      "indent": ["error", 2],
-      "jsx-quotes": ["error", "prefer-double"],
-      "arrow-parens": ["error", "as-needed"],
-      "no-var": 2,
-      "prefer-arrow-callback": 1,
-    }
-};
+    allowImportExportEverywhere: false,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    'array-callback-return': 1,
+    'no-floating-decimal': 1,
+    'init-declarations': 1,
+    'array-bracket-spacing': 1,
+    'block-spacing': 1,
+    'comma-dangle': ['error', 'always-multiline'],
+    'eol-last': 1,
+    indent: ['error', 2],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'arrow-parens': ['error', 'as-needed'],
+    'no-var': 2,
+    'prefer-arrow-callback': 1,
+    'no-empty': ['error', { allowEmptyCatch: true }],
+  },
+}
--- extensions/eslint-vue/.eslintrc.js
+++ extensions/eslint-vue/.eslintrc.js
@@ -1,27 +1,14 @@
 //更详细的配置文档请参考：https://github.com/vuejs/eslint-plugin-vue#gear-configs
 module.exports = {
-  "extends": "plugin:vue/base",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": 'module',
+  extends: 'plugin:vue/base',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'settings': {
-    'html/html-extensions': [
-      ".erb",
-      ".handlebars",
-      ".hbs",
-      ".htm",
-      ".html",
-      ".mustache",
-      ".nunjucks",
-      ".php",
-      ".tag",
-      ".twig",
-      ".wxml",
-      ".we",
-    ],
+  settings: {
+    'html/html-extensions': ['.erb', '.handlebars', '.hbs', '.htm', '.html', '.mustache', '.nunjucks', '.php', '.tag', '.twig', '.wxml', '.we'],
   },
-  "rules":{
+  rules: {
     //在computed properties中禁用异步actions
     'vue/no-async-in-computed-properties': 'error',
     //不允许重复的keys
@@ -29,9 +16,12 @@ module.exports = {
     //不允许重复的attributes
     'vue/no-duplicate-attributes': 'warn',
     //在 <template> 标签下不允许解析错误
-    'vue/no-parsing-error': ['error',{
-      'x-invalid-end-tag': false,
-    },],
+    'vue/no-parsing-error': [
+      'error',
+      {
+        'x-invalid-end-tag': false,
+      },
+    ],
     //不允许覆盖保留关键字
     'vue/no-reserved-keys': 'error',
     //强制data必须是一个带返回值的函数
@@ -52,7 +42,7 @@ module.exports = {
     'vue/require-v-for-key': 'error',
     // 检查默认的prop值是否有效
     'vue/require-valid-default-prop': 'error',
-    // 保证computed属性中有return语句 
+    // 保证computed属性中有return语句
     'vue/return-in-computed-property': 'error',
     // 强制校验 template 根节点
     'vue/valid-template-root': 'error',
@@ -62,7 +52,7 @@ module.exports = {
     'vue/valid-v-cloak': 'error',
     // 强制校验 v-else-if 指令
     'vue/valid-v-else-if': 'error',
-    // 强制校验 v-else 指令 
+    // 强制校验 v-else 指令
     'vue/valid-v-else': 'error',
     // 强制校验 v-for 指令
     'vue/valid-v-for': 'error',
@@ -84,4 +74,4 @@ module.exports = {
     'vue/valid-v-text': 'error',
     'vue/comment-directive': 0,
   },
-};
+}
